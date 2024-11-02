9c7cbe3c6200e8b0ff3b6fd7d5e3c422009f24bd
arnas <arnas@arnas.arnas>
2024-11-02 13:36:10 +0200

    move program.cs code to a class

--- src/TowerDefense.Api/Program.cs
+++ src/TowerDefense.Api/Program.cs
@@ -1,13 +1,18 @@
-// Program.cs
-var builder = WebApplication.CreateBuilder(args);
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-var appBuilder = new AppBuilder(builder);
-var app = appBuilder
-    .AddControllers()
-    .AddSignalR()
-    .SetupGameEngine()
-    .SetupAutoMapper()
-    .AddCorsPolicy()
-    .Build();
+        var appBuilder = new AppBuilder(builder);
+        var app = appBuilder
+            .AddControllers()
+            .AddSignalR()
+            .SetupGameEngine()
+            .SetupAutoMapper()
+            .AddCorsPolicy()
+            .Build();
 
-app.Run();
+        app.Run();
+    }
+}
