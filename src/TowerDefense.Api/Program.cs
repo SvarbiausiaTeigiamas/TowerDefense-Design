0d07495ba83446197294a425db773113863c419e
arnas <arnas@arnas.arnas>
2024-11-02 13:08:55 +0200

    Adding Builder

--- src/TowerDefense.Api/Program.cs
+++ src/TowerDefense.Api/Program.cs
@@ -1,55 +1,13 @@
-using TowerDefense.Api.Bootstrap;
-using TowerDefense.Api.Bootstrap.AutoMapper;
-using TowerDefense.Api.Constants;
-using TowerDefense.Api.Hubs;
-
-LoggerManager.Instance.LogInfo("Starting the application");
-
+// Program.cs
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var appBuilder = new AppBuilder(builder);
+var app = appBuilder
+    .AddControllers()
+    .AddSignalR()
+    .SetupGameEngine()
+    .SetupAutoMapper()
+    .AddCorsPolicy()
+    .Build();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddSignalR();
-builder.Services.SetupGameEngine();
-builder.Services.SetupAutoMapper();
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(Policy.DevelopmentCors, builder =>
-    {
-        builder.WithOrigins("https://localhost:3000")
-               .AllowAnyHeader()
-               .AllowAnyMethod()
-               .SetIsOriginAllowed((x) => true)
-               .AllowCredentials();
-    });
-});
-
-var app = builder.Build();
-
-LoggerManager.Instance.LogInfo("WebApp was built");
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-LoggerManager.Instance.LogInfo("Configured HTTP request pipeline");
-
-
-app.UseCors(Policy.DevelopmentCors);
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-app.MapHub<GameHub>("api/gameHub");
-
-LoggerManager.Instance.LogInfo("Running the app");
 app.Run();
