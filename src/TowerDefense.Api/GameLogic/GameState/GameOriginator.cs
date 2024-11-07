1d68d130e44bebb797e92e46f0d1d4a708e3d405
Jurgis Andziulis <jurand1@ktu.lt>
2024-11-07 04:12:33 +0200

    extended Observer

--- src/TowerDefense.Api/GameLogic/GameState/GameOriginator.cs
+++ src/TowerDefense.Api/GameLogic/GameState/GameOriginator.cs
@@ -1,7 +1,37 @@
-namespace TowerDefense.Api.GameLogic.GameState
+using TowerDefense.Api.GameLogic.Observers;
+
+namespace TowerDefense.Api.GameLogic.GameState;
+
+public static class GameOriginator
 {
-    public static class GameOriginator
+    private static readonly List<IGameObserver> Observers = new();
+
+    private static State _gameState = new();
+    public static State GameState
     {
-        public static State GameState { get; set; } = new();
+        get => _gameState;
+        set
+        {
+            _gameState = value;
+            NotifyObservers();
+        }
+    }
+
+    public static void Attach(IGameObserver observer)
+    {
+        Observers.Add(observer);
+    }
+
+    public static void Detach(IGameObserver observer)
+    {
+        Observers.Remove(observer);
+    }
+
+    private static void NotifyObservers()
+    {
+        foreach (var observer in Observers)
+        {
+            observer.Update(_gameState);
+        }
     }
 }
