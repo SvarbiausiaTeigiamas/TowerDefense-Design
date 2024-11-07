d4b393317c4378751198ef124995b79b1ce2767d
IgnasSu <150551487+IgnasSu@users.noreply.github.com>
2024-11-07 23:19:33 +0200

    Ignelis taiso (#23)
    * fixes
    
    * fixes

--- src/TowerDefense.Api/GameLogic/Grid/FirstLevelArenaGrid.cs
+++ src/TowerDefense.Api/GameLogic/Grid/FirstLevelArenaGrid.cs
@@ -3,10 +3,23 @@ namespace TowerDefense.Api.GameLogic.Grid
     public class FirstLevelArenaGrid : IArenaGrid
     {
         public GridItem[] GridItems { get; set; } = new GridItem[Constants.TowerDefense.MaxGridGridItemsForPlayer];
+        private readonly GridLayoutFactory _layoutFactory;
 
-        public FirstLevelArenaGrid()
+        // Default constructor for backward compatibility
+        public FirstLevelArenaGrid() : this(GridLayoutFactory.Instance)
         {
-            IGridLayout layoutAdapter = GridLayoutFactory.CreateLayout();
+        }
+
+        // Constructor injection for testability
+        public FirstLevelArenaGrid(GridLayoutFactory layoutFactory)
+        {
+            _layoutFactory = layoutFactory;
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            IGridLayout layoutAdapter = _layoutFactory.GetLayout();
             string gridLayout = layoutAdapter.GetFormattedLayout();
             GridItems.CreateGrid(gridLayout);
         }
