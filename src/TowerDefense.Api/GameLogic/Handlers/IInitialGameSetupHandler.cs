e9c48ce8ec4f39c07ba263052c6a62b3b471e736
IgnasSu <150551487+IgnasSu@users.noreply.github.com>
2024-12-16 13:50:55 +0200

    fixes

---
+++ src/TowerDefense.Api/GameLogic/Handlers/IInitialGameSetupHandler.cs
@@ -0,0 +1,15 @@
+﻿using TowerDefense.Api.GameLogic.Player;
+
+namespace TowerDefense.Api.GameLogic.Handlers
+{
+    public interface IInitialGameSetupHandler
+    {
+        IPlayer AddNewPlayer(string playerName);
+        void SetConnectionIdForPlayer(string playerName, string connectionId);
+        IPlayer AddPlayerToGame(string playerName);
+        void SetArenaGridForPlayer(IPlayer player);
+        void SetShopForPlayer(IPlayer player);
+        void SetPerkStorageForPlayer(IPlayer player);
+        Task TryStartGame();
+    }
+}
